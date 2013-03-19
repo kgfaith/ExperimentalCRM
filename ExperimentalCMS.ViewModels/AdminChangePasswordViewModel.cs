3d0b9dadf0df7c9cc02c148f5f7a5791fdffa7a7
Kyaw Kyaw Win <kg.faith@gmail.com>
2013-03-19 21:11:44 +0000

    Minor changes2
    Minor changes2

--- ExperimentalCMS.ViewModels/AdminChangePasswordViewModel.cs
+++ ExperimentalCMS.ViewModels/AdminChangePasswordViewModel.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ExperimentalCMS.Model;
 
 namespace ExperimentalCMS.ViewModels
 {
@@ -11,11 +12,15 @@ namespace ExperimentalCMS.ViewModels
     {
         public int AdminId { get; set; }
 
-        [Required]
-        [StringLength(100)]
-        [DataType(DataType.Password)]
-        [Display(Name = "Old Password")]
-        public string OldPassword { get; set; }
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [Display(Name = "Name")]
+        public string FullName { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email address")]
+        public string Email { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
@@ -27,5 +32,15 @@ namespace ExperimentalCMS.ViewModels
         [Display(Name = "Confirm password")]
         [Compare("NewPassword", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmNewPassword { get; set; }
+
+        public void TransformFromAdminObject(Admin admin)
+        {
+            AdminId = admin.AdminId;
+            UserName = admin.UserName;
+            FullName = admin.FirstName + " " + admin.LastName;
+            Email = admin.Email;
+        }
+
+
     }
 }
