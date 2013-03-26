09837a25331653b14a1930479155505bbbe8a5f7
Kyaw Kyaw Win <kg.faith@gmail.com>
2013-03-26 16:46:52 +0000

    Article manager added
    IArticlemanager and Articlemanager are added and Insert method is in
    progress.

---
+++ Domain/Contracts/IArticleManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExperimentalCMS.Domain.Utility;
+using ExperimentalCMS.Model;
+
+namespace ExperimentalCMS.Domain.Contracts
+{
+    public interface IArticleManager
+    {
+        DomainResponse<Article> CreateNewArticle(Article newArticle);
+        DomainResponse<BooleanResult> EditArticle(Article article);
+        IEnumerable<Article> GetAllArticleList();
+        DomainResponse<Article> GetArticleById(int id);
+        bool DeleteArticleById(int id);
+        void Dispose();
+    }
+}
---
+++ Domain/Managers/ArticleManager.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExperimentalCMS.Domain.Contracts;
+using ExperimentalCMS.Domain.Utility;
+using ExperimentalCMS.Model;
+using ExperimentalCMS.Repositories;
+
+namespace ExperimentalCMS.Domain.Managers
+{
+    public class ArticleManager : IArticleManager
+    {
+        private IUnitOfWork _uOW;
+
+        public ArticleManager(IUnitOfWork uow)
+        {
+            _uOW = uow;
+        }
+
+        public DomainResponse<Article> CreateNewArticle(Article newArticle)
+        {
+            var response = new DomainResponse<Article>();
+
+            try
+            {
+                newArticle = _uOW.ArticleRepo.Insert(newArticle);
+                _uOW.Save();
+            }
+            catch (Exception ex)
+            {
+                return response.ReturnFailResponse(new[] { ex.Message }
+                       , "There is an error trying to update data"
+                       , null);
+            }
+
+            if (newArticle.ArticleId > 0)
+            {
+
+                return response.ReturnSuccessResponse(newArticle
+                        , new[] { "Admin data has been successfully updated." }
+                        , "Admin data has been successfully updated.");
+            }
+            else
+            {
+                return response.ReturnFailResponse(new[] { "Error occur while trying to insert new article" }
+                       , "There is an error trying to save data"
+                       , null);
+            }
+        }
+
+        public DomainResponse<BooleanResult> EditArticle(Article article)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Article> GetAllArticleList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DomainResponse<Article> GetArticleById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteArticleById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
