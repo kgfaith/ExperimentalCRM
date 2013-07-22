a20a63c3b7c3f9158d371156fa8d3b33cdbb0376
Kyaw Kyaw Win <kg.faith@gmail.com>
2013-07-23 00:10:52 +0100

    PlaceManager Create and Edit methods.

--- Domain/Managers/PlaceManager.cs
+++ Domain/Managers/PlaceManager.cs
@@ -23,45 +23,116 @@ namespace ExperimentalCMS.Domain.Managers
         {
             var response = new DomainResponse<Place>();
             var tempRelatedPlaces = newPlace.RelatedPlaces;
+            var tempArticles = newPlace.Articles;
             newPlace.RelatedPlaces = new List<Place>();
-            return null;
-            //newPlace.LastUpdatedDate = DateTime.Now;
-            //try
-            //{
-            //    newArticle = _uOW.ArticleRepo.Insert(newArticle);
-            //    _uOW.Save();
-            //    foreach (Place place in tempRelatedPlaces)
-            //    {
-            //        var tempPlace = _uOW.PlaceRepo.GetByID(place.PlaceId);
-            //        newArticle.Places.Add(tempPlace);
-            //    }
-            //    _uOW.Save();
-            //}
-            //catch (Exception ex)
-            //{
-            //    return response.ReturnFailResponse(new[] { ex.Message }
-            //           , "There is an error trying to create a new article."
-            //           , null);
-            //}
+            newPlace.Articles = new List<Article>();
 
-            //if (newArticle.ArticleId > 0)
-            //{
+            try
+            {
+                newPlace = _uOW.PlaceRepo.Insert(newPlace);
+                _uOW.Save();
+                foreach (Place place in tempRelatedPlaces)
+                {
+                    var tempPlace = _uOW.PlaceRepo.GetByID(place.PlaceId);
+                    newPlace.RelatedPlaces.Add(tempPlace);
+                }
+                foreach (Article article in tempArticles)
+                {
+                    var tempArticle = _uOW.ArticleRepo.GetByID(article.ArticleId);
+                    newPlace.Articles.Add(tempArticle);
+                }
+                _uOW.Save();
+            }
+            catch (Exception ex)
+            {
+                return response.ReturnFailResponse(new[] { ex.Message }
+                       , "There is an error trying to create a new article."
+                       , null);
+            }
 
-            //    return response.ReturnSuccessResponse(newArticle
-            //            , new[] { "New article has been successfully created." }
-            //            , "New article has been successfully created.");
-            //}
-            //else
-            //{
-            //    return response.ReturnFailResponse(new[] { "Error occur while trying to create new article" }
-            //           , "There is an error trying to save data"
-            //           , null);
-            //}
+            if (newPlace.PlaceId > 0)
+            {
+
+                return response.ReturnSuccessResponse(newPlace
+                        , new[] { "New article has been successfully created." }
+                        , "New article has been successfully created.");
+            }
+            else
+            {
+                return response.ReturnFailResponse(new[] { "Error occur while trying to create new article" }
+                       , "There is an error trying to save data"
+                       , null);
+            }
         }
 
-        public DomainResponse<BooleanResult> EditPlace(Place place)
+        public DomainResponse<BooleanResult> EditPlace(Place newPlace)
         {
-            throw new NotImplementedException();
+            var response = new DomainResponse<BooleanResult>();
+            try
+            {
+                var placeData = _uOW.PlaceRepo.Get(filter: p => p.PlaceId == newPlace.PlaceId, includeProperties: "RelatedPlaces,Articles").FirstOrDefault();
+                placeData.PlaceName = newPlace.PlaceName ?? placeData.PlaceName;
+                placeData.Description = newPlace.Description ?? placeData.Description;
+                placeData.InternalRanking = newPlace.InternalRanking > -1 ? newPlace.InternalRanking : placeData.InternalRanking;
+                placeData.PlaceTypeId = newPlace.PlaceTypeId > 0 ? newPlace.PlaceTypeId : placeData.PlaceTypeId;
+                placeData.PlaceType = newPlace.PlaceType != null ? newPlace.PlaceType : placeData.PlaceType;
+
+                UpdateRelatedPlaces(newPlace, placeData);
+                UpdateRelatedArticles(newPlace, placeData);
+                _uOW.PlaceRepo.Update(placeData);
+                _uOW.Save();
+            }
+
+            catch (Exception ex)
+            {
+                return response.ReturnFailResponse(new[] { ex.Message }
+                       , "There is an error trying to update data"
+                       , new BooleanResult { Success = false });
+            }
+
+            return response.ReturnSuccessResponse(new BooleanResult { Success = true }
+                    , new[] { "Admin data has been successfully updated." }
+                    , "Admin data has been successfully updated.");
+        }
+
+        private void UpdateRelatedPlaces(Place newPlace, Place placeToUpdate)
+        {
+            var places = _uOW.PlaceRepo.Get();
+            var selectedIdsAry = newPlace.RelatedPlaces.Select(x => x.PlaceId).ToArray();
+            var existingIdsAry = placeToUpdate.RelatedPlaces.Select(x => x.PlaceId).ToArray();
+            foreach (Place place in places)
+            {
+                if (selectedIdsAry.Contains(place.PlaceId))
+                {
+                    if (!existingIdsAry.Contains(place.PlaceId))
+                        placeToUpdate.RelatedPlaces.Add(place);
+                }
+                else
+                {
+                    if (existingIdsAry.Contains(place.PlaceId))
+                        placeToUpdate.RelatedPlaces.Remove(place);
+                }
+            }
+        }
+
+        private void UpdateRelatedArticles(Place newPlace, Place placeToUpdate)
+        {
+            var articles = _uOW.ArticleRepo.Get();
+            var selectedIdsAry = newPlace.Articles.Select(x => x.ArticleId).ToArray();
+            var existingIdsAry = placeToUpdate.Articles.Select(x => x.ArticleId).ToArray();
+            foreach (Article article in articles)
+            {
+                if (selectedIdsAry.Contains(article.ArticleId))
+                {
+                    if (!existingIdsAry.Contains(article.ArticleId))
+                        placeToUpdate.Articles.Add(article);
+                }
+                else
+                {
+                    if (existingIdsAry.Contains(article.ArticleId))
+                        placeToUpdate.Articles.Remove(article);
+                }
+            }
         }
 
         public DomainResponse<IEnumerable<PlaceType>> GetPlaceTypeList()
