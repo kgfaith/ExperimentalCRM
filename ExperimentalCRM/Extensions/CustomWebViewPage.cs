cd3336a50ee1cd460b9ebe2cf1678a6c9055e0e1
Kyaw Kyaw Win <kg.faith@gmail.com>
2013-07-30 15:57:56 +0100

    added custom webviewpage by mistake

---
+++ ExperimentalCRM/Extensions/CustomWebViewPage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ExperimentalCMS.Web.BackEnd.Infrastructure;
+
+namespace ExperimentalCMS.Web.BackEnd.Extensions
+{
+    //public class CustomWebViewPage
+    //{
+    //}
+
+    public abstract class CustomWebViewPage : WebViewPage
+    {
+        public virtual new CustomPrincipal User
+        {
+            get { return base.User as CustomPrincipal; }
+        }
+    }
+
+    public abstract class CustomWebViewPage<TModel> : WebViewPage<TModel>
+    {
+        public virtual new CustomPrincipal User
+        {
+            get { return base.User as CustomPrincipal; }
+        }
+    }
+}
